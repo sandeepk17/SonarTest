f0a677f2700f0fbeee0fc097c8e00650834cdb37
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-02-10 15:44:01 +0100

    Test edit - branch

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hi there as well!");
+            int a = 1;
         }
     }
 }
